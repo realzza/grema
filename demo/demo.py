75c6a04f47fc007657d0e17a0eb87ab45484142b
ziang <zz188@duke.edu>
2021-10-01 17:38:35 -0400

    clean code

--- demo/demo.py
+++ demo/demo.py
@@ -10,9 +10,7 @@ from time import localtime, strftime
 def parse_args():
   desc = "parse args for memorizing gre words"
   parser = argparse.ArgumentParser(description=desc)
-  # parser.add_argument("--day", type=int, required=True, help="select from 1 to 12")
   parser.add_argument('--preview', type=int, default=10, help="peek words from list [default: 10]")
-  # parser.add_argument('--count', type=int, default=300, help="number of words to memorize from current list")
   parser.add_argument('--reset', action='store_true', default=False, help="reset learning history with CAUTION [default: False]")
   parser.add_argument('--update', action='store_true', default=True, help="Want the learning history to be updated or not? [default: True]")
   parser.add_argument('--log', type=str, default="./notes/", help="directory to restore generated learning notes [default: ./notes/]")
@@ -32,13 +30,9 @@ if __name__ == "__main__":
   # parse args
   args = parse_args()
 
-  # check day in range
-  # thisDay = args.day 
-
   # check if preview/peek required
   preview = args.preview
   wordListName = "demo.xlsx"
-  # print(wordListName)
   allWords = pd.read_excel(wordListName)
   print("... demo wordlist loaded ...")
   print(allWords.head(preview))
